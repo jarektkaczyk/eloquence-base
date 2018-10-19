e9bbdb104bd13781682267f99316b92496f54e23
David Cox <dave.cox@dunntire.com>
2018-10-19 10:50:19 -0400

    remove the override for aggregate query creation

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -3,51 +3,10 @@
 namespace Sofa\Eloquence\Query;
 
 use Sofa\Eloquence\Subquery;
+use Illuminate\Database\Query\Builder as BaseBuilder;
 
-class Builder extends \Illuminate\Database\Query\Builder
+class Builder extends BaseBuilder
 {
-    /**
-     * Execute an aggregate function on the database.
-     *
-     * @param  string  $function
-     * @param  array   $columns
-     * @return float|int
-     */
-    public function aggregate($function, $columns = ['*'])
-    {
-        $this->aggregate = compact('function', 'columns');
-
-        $previousColumns = $this->columns;
-
-        if (!$this->from instanceof Subquery) {
-            // We will also back up the select bindings since the select clause will be
-            // removed when performing the aggregate function. Once the query is run
-            // we will add the bindings back onto this query so they can get used.
-            $previousSelectBindings = $this->bindings['select'];
-
-            $this->bindings['select'] = [];
-        }
-
-        $results = $this->get($columns);
-
-        // Once we have executed the query, we will reset the aggregate property so
-        // that more select queries can be executed against the database without
-        // the aggregate value getting in the way when the grammar builds it.
-        $this->aggregate = null;
-
-        $this->columns = $previousColumns;
-
-        if (!$this->from instanceof Subquery) {
-            $this->bindings['select'] = $previousSelectBindings;
-        }
-
-        if (isset($results[0])) {
-            $result = array_change_key_case((array) $results[0]);
-
-            return $result['aggregate'];
-        }
-    }
-
     /**
      * Backup some fields for the pagination count.
      *
